167a632bc7fd1a2f528ba6337d61ec0b4d4da257
Alejandro Bastidas <manuba2309@gmail.com>
2025-10-29 07:57:15 -0500

    filtrado de series con pandas

--- Python_Data_Science_Machine_Learning_Udemy/PANDAS/pandas_filtrar_series.py
+++ Python_Data_Science_Machine_Learning_Udemy/PANDAS/pandas_filtrar_series.py
@@ -1,9 +1,38 @@
-print("ejemplo")
-import pandas as pd 
-datos = {"Pais": ["Noruega", "Colombia"], "Habitantes": [23,45]}
-print(type(datos))
+import pandas as pd
 
-df = pd.DataFrame(datos)
-print(df.head())
+#Filtrado de series: Hacerle preguntas especificas a nuestros datos
 
-print(df.notnull().sum())
+serie = pd.Series([5,10,15,20,25])
+print(serie)
+
+#FILTRADO DE NUMEROS
+
+#Cuales elementos de mi serie son mayore a un numero especifico
+filtro = serie > 15 #Filtro me queda con valores booleanos que cumplen con la condicion
+print("Tipo de filtro: " + str(type(filtro)) + "Filtro: \n" + str(filtro))
+
+#Filtramos la serie que cumplan con el filtro
+serie_filtrada = serie[filtro] 
+print("Serie filtrada")
+print(serie_filtrada)
+
+# FILTRADO DE STRINGS
+
+serie_2 = pd.Series(["Melon", "Banana", "Papaya"])
+print(serie_2)
+print(type(serie_2))
+
+filtro_2 = serie_2.str.contains("Melon")
+print(filtro_2)
+serie_filtrada = serie_2[filtro_2]
+print(serie_filtrada)
+
+
+
+
+
+
+
+
+
+
